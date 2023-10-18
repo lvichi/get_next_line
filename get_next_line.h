3c9de1d7f07ede25d9f26e2ef440b1e7bbe8fb60
lvichi <lvichi@student.42porto.com>
2023-10-18 18:43:01 +0100

    Starting the project get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lvichi <lvichi@student.42porto.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 18:34:43 by lvichi            #+#    #+#             */
+/*   Updated: 2023/10/18 18:39:48 by lvichi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+
+# define BUFFER_SIZE 1
+
+char	*get_next_line(int fd);
+
+#endif
